fb3144721a5e4f47264649f2be36418567f83932
gwen <gwan1017@gmail.com>
2026-02-27 01:13:35 +0800

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // 開發中、產品路徑
+  base: process.env.NODE_ENV === 'production' ? '/2025-HexSchool-React---week6/' : '/',
   plugins: [react()],
 })
